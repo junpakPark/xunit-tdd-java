d310f0805bd7e8d7f01ffda0cbfc6d2142c8ead0
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 15:52:31 +0900

    refactor(1단계): templateMethod 패턴 적용

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -3,9 +3,7 @@ package xunit;
 public class XUnitTest {
 
     public static void main(String[] args) {
-        new TestCaseTest("testRunning").run();
-        new TestCaseTest("testSetUp").run(); // 테스트 간 격리를 위하여 별도의 객체로 실행
-
+        new TestCaseTest("testTemplateMethod").run();
     }
 
 }
