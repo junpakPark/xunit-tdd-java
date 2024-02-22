4ce16fa2380ae925ca12c58e2d211a33ab27f399
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 14:29:20 +0900

    feat: 메서드별 테스트 여부를 알려주는 기능(실패)

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -3,7 +3,15 @@ package xunit;
 public class WasRun {
     public boolean wasRun;
 
+    public WasRun(final String name) {
+
+    }
+
     public void testMethod() {
         wasRun = true;
+    }
+
+    public void run() {
+        
     }
 }
