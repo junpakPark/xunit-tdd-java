cad5ac68ff2a5bd25d3c17cd11e2e3db2fb7cebe
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 14:22:03 +0900

    feat: 테스트 여부를 알려주는 기능(실패)

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -2,6 +2,9 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-
+        WasRun wasRun = new WasRun();
+        System.out.println(wasRun.wasRun); // false
+        wasRun.testMethod();
+        System.out.println(wasRun.wasRun); // true
     }
 }
