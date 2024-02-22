5082d2c8f29b6a9604353897c910eaf861620bd9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 14:35:43 +0900

    refactor: (1단계) TestCase Super Class 분리

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -3,25 +3,16 @@ package xunit;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class WasRun {
+public class WasRun extends TestCase {
 
-    private final String name;
     public boolean wasRun;
 
     public WasRun(final String name) {
-        this.name = name;
+        super(name);
     }
 
     public void testMethod() {
         wasRun = true;
     }
 
-    public void run() {
-        try {
-            Method method = getClass().getMethod(name);
-            method.invoke(this);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
