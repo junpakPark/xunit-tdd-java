768057130c2c9335d584bdf95a6a595928dafecd
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 15:25:29 +0900

    refactor(setUp): WasRun 객체 생성 코드 중복 제거

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -1,19 +1,25 @@
 package xunit;
 
 public class TestCaseTest extends TestCase {
+
+    private WasRun wasRun;
+
     public TestCaseTest(final String name) {
         super(name);
     }
 
+    @Override
+    public void setUp() {
+        wasRun = new WasRun("testMethod");
+    }
+
     public void testRunning() {
-        WasRun wasRun = new WasRun("testMethod");
         Assert.assertEquals(false, wasRun.wasRun);
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasRun);
     }
 
     public void testSetUp() {
-        WasRun wasRun = new WasRun("testMethod");
         Assert.assertEquals(false, wasRun.wasSetUp);
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasSetUp);
