4166bf2a771d4378a00b80afcef4ef34e3d04c82
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 20:34:33 +0900

    refactor(4단계): reflection으로 test로 시작하는 메서드 자동으로 불러오기

--- src/xunit/TestSuite.java
+++ src/xunit/TestSuite.java
@@ -1,11 +1,28 @@
 package xunit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class TestSuite implements Test{
+public class TestSuite implements Test {
 
     List<Test> tests = new ArrayList<>();
+
+    public TestSuite() {
+    }
+
+    public TestSuite(final Class<? extends TestCase> testClass) {
+        Arrays.stream(testClass.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        add(testClass.getConstructor(String.class).newInstance(method.getName()));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+    }
 
     public void add(final Test test) {
         tests.add(test);
