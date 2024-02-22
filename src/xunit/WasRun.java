fd7c4453c1bee203448f811c01875039799d4ac7
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-02-22 14:32:49 +0900

    feat: 메서드별 테스트 여부를 알려주는 기능(성공)

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -1,10 +1,15 @@
 package xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
+
+    private final String name;
     public boolean wasRun;
 
     public WasRun(final String name) {
-
+        this.name = name;
     }
 
     public void testMethod() {
@@ -12,6 +17,11 @@ public class WasRun {
     }
 
     public void run() {
-        
+        try {
+            Method method = getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
